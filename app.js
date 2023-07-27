9c78ed1738d9ffdc45ea2cf0eee49bbae15c231a
RafaelAliani <rafael.anima.edu.uy>
2023-07-27 08:41:03 -0300

    Corrección del import bodyParser

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { router } from "./src/routers/router.js";
 
 const app = express();
-app.use(bodyParser.json());
 
 app.use("/", router);
 
