0dad55c8c90db598c8c86839a5c9ae2cf92cd710
RafaelAliani <138121888+RafaelAliani@users.noreply.github.com>
2023-08-01 17:59:06 -0300

    Cambios  la ruta de perfiles y ajustes en comprobar variables de entorno

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { router } from "./src/routers/router.js";
 import { checkPort } from "./src/config/enviroment-comprobate.js";
+import { checkUrl } from "./src/config/enviroment-comprobate.js";
 import "dotenv/config";
-//require("dotenv").config();
 
 checkPort(process.env.PORT);
+checkUrl(process.env.URL);
 
 const port = process.env.PORT;
 const app = express();
--- src/config/enviroment-comprobate.js
+++ src/config/enviroment-comprobate.js
@@ -1,7 +1,16 @@
 export const checkPort = (port) => {
-  if (!port || port.trim() === "") {
+  if (!port || !port.trim() === "") {
     console.error(
       "La variable de entorno PORT no está definida en el archivo .env o está vacía."
+    );
+    process.exit(1);
+  }
+};
+
+export const checkUrl = (url) => {
+  if (!url || !url.trim() === "") {
+    console.error(
+      "La variable de entorno URL no está definida en el archivo .env o está vacía."
     );
     process.exit(1);
   }
