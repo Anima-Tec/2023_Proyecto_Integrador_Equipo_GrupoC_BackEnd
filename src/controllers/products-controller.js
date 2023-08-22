dc69d7d7c6d8476c3c5195dcc12807e293bba44a
RafaelAliani <138121888+RafaelAliani@users.noreply.github.com>
2023-08-22 10:13:06 -0300

    arreglos a products-controller

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -3,8 +3,22 @@ const products = [
   { id: 2, name: "Product 2", price: 19.99 },
 ];
 
-const getProducts = (req, res) => {
+export const getProducts = (req, res) => {
   res.json(products);
 };
 
-export { getProducts };
+export const createProduct = (req, res) => {
+  const { name, price } = req.params;
+
+  if (!name || !price) {
+    return res
+      .status(400)
+      .json({ error: "Todos los campos son obligatorios." });
+  }
+
+  const existingProduct = products.find((name) => product.name === name);
+
+  if (existingProduct) {
+    return res.status(409).json({ error: "El producto ya existe" });
+  }
+};
