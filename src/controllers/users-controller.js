9b25ebda76fda5d281c845d9c5d92dc66f0f756e
RafaelAliani <138121888+RafaelAliani@users.noreply.github.com>
2023-08-23 12:47:59 -0300

    primeros cambios y archivo schema.prisma

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -1,12 +1,13 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const users = [
-  { id: 1, name: "Jhon Doe", email: "jhon@gmail.com", password: "12345678" },
-  { id: 2, name: "Jane Doe", email: "jane@gmail.com", password: "abcdefg" },
-];
+// const users = [
+//   { id: 1, name: "Jhon Doe", email: "jhon@gmail.com", password: "12345678" },
+//   { id: 2, name: "Jane Doe", email: "jane@gmail.com", password: "abcdefg" },
+// ];
 
-export const getUsers = (req, res) => {
+export const getUsers = async (req, res) => {
+  const users = await prisma.user.findMany;
   res.json(users);
 };
 
@@ -34,59 +35,44 @@ export const getUserProfile = (req, res) => {
   res.json(`name: ${user.name} email: ${user.email}`);
 };
 
-export const createUser = (req, res) => {
-  const { email, password, name } = req.params;
+export const createUser = async (req, res) => {
+  // const { email, password, name } = req.params;
+  const { email, password, name } = req.body;
 
-  if (!email || !password || !name) {
-    return res
-      .status(400)
-      .json({ error: "Todos los campos son obligatorios." });
-  }
-
-  // Verificamos si el email ya existe en la lista de usuarios
-  const existingUser = users.find((user) => user.email === email);
-
-  if (existingUser) {
-    return res.status(403).json({ error: "El email ya está registrado." });
-  }
-
-  if (!email.includes("@")) {
-    return res.status(400).json({ error: "email invalido" });
-  }
-
-  if (password.length < 8) {
-    return res.status(400).json({ error: "contraseña incorrecta" });
-  }
-
-  // Generamos un nuevo ID para el usuario
-  const id = users.length + 1;
-
-  // Creamos el nuevo usuario
-  const newUser = {
-    id,
-    name,
-    email,
-    password,
-  };
-
-  // Agregamos el nuevo usuario a la lista
-  users.push(newUser);
+  const newUser = await prisma.user.create({
+    data: {
+      email,
+      password,
+      name,
+    },
+  });
 
   res.status(201).json(newUser);
 };
 
-export const deleteUser = (req, res) => {
-  const id = parseInt(req.params.id);
+export const deleteUserById = async (req, res) => {
+  // const id = parseInt(req.params.id);
+  const userId = parseInt(req.params.id);
 
-  //Se busca el usuario
-  const user = users.findIndex((user) => user.id === id);
-
-  //Se confirma que exista en la base de datos
-  if (user === -1) {
-    res.status(404).json({ error: "Usuario no encontrado" });
+  if (userId === -1) {
+    return res.status(406).json({ error: "ID invalida" });
   }
 
+  //Se busca el usuario
+  if (!userId) {
+    return res.status(404).json({ error: "Id no encontrada" });
+  }
+
+  // //Se confirma que exista en la base de datos
+  // if (user === -1) {
+  //   res.status(404).json({ error: "Usuario no encontrado" });
+  // }
+
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: userId,
+    },
+  });
   //Se elimina el usuario
-  users.splice(user, 1);
-  res.status(200).json({ message: "Usuario eliminado correctamente" });
+  res.status(200).json(deleteUser);
 };
