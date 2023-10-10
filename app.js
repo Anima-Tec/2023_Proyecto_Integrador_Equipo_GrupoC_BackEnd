7708bceab49e050a480160257213c3c5acb64af4
Onesegen7 <138121888+RafaelAliani@users.noreply.github.com>
2023-10-10 09:20:16 -0300

    Creación del token JWT

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import { router } from "./src/routers/router.js";
-import { checkPort } from "./src/config/enviroment-comprobate.js";
+import { checkKey, checkPort } from "./src/config/enviroment-comprobate.js";
 import { checkUrl } from "./src/config/enviroment-comprobate.js";
 import "dotenv/config";
 
 //Se compueba si existe las variables de entorno que se necesitan para correr el programa
 checkPort(process.env.PORT);
 checkUrl(process.env.DATABASE_URL);
+checkKey(process.env.JWT_ACCESS_SECRET);
 
 const port = process.env.PORT;
 
 const app = express();
 
+app.use();
 app.use(express.json());
 app.use("/", router);
 
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { generateToken } from "../util/authUtils";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    res.status(401).json({ message: "Token no proporcionado" });
+  }
+
+  jwt.verify(token, generateToken, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ message: "Token inavalido" });
+    }
+
+    req.usuarioId = decoded.indexOf;
+    next();
+  });
+};
